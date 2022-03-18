a1ca237622e00db6fe1487a073cfffb323fd0fc5
zsqan <zsqandzyr@gmail.com>
2022-03-18 21:47:34 +0800

    登录相关

--- app/src/main/java/com/able/playandroidmvi/ui/MainActivity.kt
+++ app/src/main/java/com/able/playandroidmvi/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.able.playandroidmvi
+package com.able.playandroidmvi.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.able.playandroidmvi.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
